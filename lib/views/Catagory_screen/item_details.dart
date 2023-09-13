23b6766346f1f82549fcbb0bd587c12904795b60
sagor <sagorsamadder.official@gmail.com>
2023-09-13 18:30:50 +0600

    update item details page

--- lib/views/Catagory_screen/item_details.dart
+++ lib/views/Catagory_screen/item_details.dart
@@ -324,18 +324,22 @@ class ItemDetails extends StatelessWidget {
                     child: ourButton(
                         color: redColor,
                         onPress: () {
-                          controller.addToCart(
-                              color: data['p_colors']
-                                  [controller.colorIndex.value],
-                              context: context,
-                              vendorID: data['vendor_id'],
-                              img: data['p_imgs'][0],
-                              qty: controller.quantity.value,
-                              sellername: data['p_seller'],
-                              title: data['p_name'],
-                              tprice: controller.totalPrice.value);
-                          VxToast.show(context,
-                              msg: "Added to cart sucessfully");
+                          if (controller.quantity.value > 0) {
+                            controller.addToCart(
+                                color: data['p_colors']
+                                    [controller.colorIndex.value],
+                                context: context,
+                                vendorID: data['vendor_id'],
+                                img: data['p_imgs'][0],
+                                qty: controller.quantity.value,
+                                sellername: data['p_seller'],
+                                title: data['p_name'],
+                                tprice: controller.totalPrice.value);
+                            VxToast.show(context,
+                                msg: "Added to cart sucessfully");
+                          } else {
+                            VxToast.show(context, msg: "Please add quantity");
+                          }
                         },
                         textcolor: whiteColor,
                         title: "Add to cart"),
