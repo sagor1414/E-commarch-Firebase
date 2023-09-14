0a74cf1b9883936bafea79104afb24df0f181888
sagor <sagorsamadder.official@gmail.com>
2023-09-14 17:26:00 +0600

    complete message screen

--- lib/views/chat_screen/messageing_screen.dart
+++ lib/views/chat_screen/messageing_screen.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:srss_app/consts/consts.dart';
 import 'package:srss_app/services/firestore_services.dart';
+import 'package:srss_app/views/chat_screen/chat_screen.dart';
 import 'package:srss_app/widzet_common/loading_indicator.dart';
 
 class MessagesScreen extends StatelessWidget {
@@ -28,8 +29,47 @@ class MessagesScreen extends StatelessWidget {
                   .fontFamily(semibold)
                   .makeCentered();
             } else {
-              return Container(
-                color: redColor,
+              var data = snapshot.data!.docs;
+              return Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  children: [
+                    Expanded(
+                        child: ListView.builder(
+                            itemCount: data.length,
+                            itemBuilder: (BuildContext context, int index) {
+                              return Card(
+                                child: ListTile(
+                                  onTap: () {
+                                    Get.to(
+                                      () => const ChatScreen(),
+                                      arguments: [
+                                        data[index]['friend_name'],
+                                        data[index]['toId'],
+                                      ],
+                                    );
+                                  },
+                                  leading: const CircleAvatar(
+                                    backgroundColor: redColor,
+                                    child: Icon(
+                                      Icons.person,
+                                      color: whiteColor,
+                                    ),
+                                  ),
+                                  title: "${data[index]['friend_name']}"
+                                      .text
+                                      .fontFamily(semibold)
+                                      .color(darkFontGrey)
+                                      .make(),
+                                  subtitle: "${data[index]['last_msg']}"
+                                      .text
+                                      .color(darkFontGrey)
+                                      .make(),
+                                ),
+                              );
+                            }))
+                  ],
+                ),
               );
             }
           }),
